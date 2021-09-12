4e3ced6bcc8fb0018dce264b4b8cec0b385b1bf9
Seb <75587701+BobEsponjeYT@users.noreply.github.com>
2021-09-12 20:08:03 -0300

    Update example1.py

--- handlers/commands/example1.py
+++ handlers/commands/example1.py
@@ -1,1 +1,11 @@
 
+import discord
+from discord.ext import commands
+import asyncio
+
+async def run(ctx=None):
+  await ctx.send("Example")
+  await asyncio.sleep(5)
+  msg = await ctx.send("Example")
+  await asyncio.sleep(5)
+  await msg.delete()
