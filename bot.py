d855a2e443c95697c65d9067f82a83e4ed5b311e
Seb <75587701+BobEsponjeYT@users.noreply.github.com>
2021-09-12 20:06:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+from handlers.commands import *
 
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+  pass
+
+## Commands
+
+@bot.command() ## Simple command
+async def one(ctx):
+  await example1.run(ctx=ctx)
+  
+  
+bot.run("TOKEN")
